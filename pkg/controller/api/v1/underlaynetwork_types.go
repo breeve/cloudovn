2a1f3fe2984d37fb1763c869a905f792e79c8bda
zhangfeng5 <zhangfeng5@sensetime.com>
2026-02-24 15:45:14 +0800

    underlay network

--- pkg/controller/api/v1/underlaynetwork_types.go
+++ pkg/controller/api/v1/underlaynetwork_types.go
@@ -20,46 +20,23 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // UnderlayNetworkSpec defines the desired state of UnderlayNetwork
 type UnderlayNetworkSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of UnderlayNetwork. Edit underlaynetwork_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// DefaultInterface
+	DefaultInterface string `json:"defaultInterface,omitempty"`
 }
 
 // UnderlayNetworkStatus defines the observed state of UnderlayNetwork.
 type UnderlayNetworkStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the UnderlayNetwork resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
+	ResourceStatus `json:",inline"`
 }
 
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state`
 
 // UnderlayNetwork is the Schema for the underlaynetworks API
 type UnderlayNetwork struct {
